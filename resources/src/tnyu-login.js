41a58881924927bc885fa08ee23120c4e1a3630f
Abhi Agarwal <me@abhiagarwal.com>
2015-06-01 17:58:33 -0400

    Adding attributes

--- resources/src/tnyu-login.js
+++ resources/src/tnyu-login.js
@@ -3,9 +3,9 @@ $.getJSON("http://wiki.techatnyu.org/api.php?action=query&meta=userinfo&format=j
         if (!userStatus.error) return;
         $.getJSON("https://api.tnyu.org/v2/people/me")
             .done(function(user) {
-                if (user.data && user.data.roles && user.data.roles.length != 0) {
-                    var userName = user.data.name.replace(/\s/g, '');
-                    var userPass = sha1(sha1(sha1(user.data.id), sha1(user.data.facebookId)), sha1(user.data.created));
+                if (user.data && user.data.attributes.roles && user.data.attributes.roles.length != 0) {
+                    var userName = user.data.attributes.name.replace(/\s/g, '');
+                    var userPass = sha1(sha1(sha1(user.data.id), sha1(user.data.attributes.facebookId)), sha1(user.data.attributes.created));
                     wiki_auth(userName, userPass, "/");
                 }
             });
